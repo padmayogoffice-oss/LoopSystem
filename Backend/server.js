62169dc95ec26089fb1156aab2e0d5ab1abd2c23
krushna <krishnagate89@gmail.com>
2026-03-24 11:06:24 +0530

    update package.json and server.js file according to deployment

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,11 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
 
 // Request logging middleware
 app.use((req, res, next) => {
